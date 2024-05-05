4804e675a6719e18f10929b6fc28dacfc1188d39
devAYZ <fatokimiemman@gmail.com>
2024-05-05 11:30:45 +0100

    use ExpressibleByFloatLiteral

--- PhantomTypes.playground/Pages/Money Example 2.xcplaygroundpage/Contents.swift
+++ PhantomTypes.playground/Pages/Money Example 2.xcplaygroundpage/Contents.swift
@@ -7,35 +7,40 @@ protocol Currency: Equatable {
     var amount: CurrencyValue<Self> { get set }
 }
 
-struct CurrencyValue<P: Currency>: Equatable {
-    var amount: Double = 0
+struct CurrencyValue<P: Currency>: Equatable, ExpressibleByFloatLiteral {
+    var value: Double = 0
+    
+    init(floatLiteral value: FloatLiteralType) {
+        self.value = value
+    }
 }
 
 struct PoundsSterling: Currency {
     var code: String { "GBP" }
     
-    var amount = CurrencyValue<Self>()
+    var amount: CurrencyValue<Self>
 }
 
 struct Euro: Currency {
     var code: String { "EUR" }
     
-    var amount = CurrencyValue<Self>()
+    var amount: CurrencyValue<Self>
 }
 
 struct Dollar: Currency {
     var code: String { "USD" }
     
-    var amount = CurrencyValue<Self>()
+    var amount: CurrencyValue<Self>
 }
 
 class City {
     
-    var dollarEarning = Dollar(amount: .init(amount: 1000))
-    var poundEarning = PoundsSterling(amount: .init(amount: 1000))
+    var dollarEarning = Dollar(amount: 1000.0)
+    var poundEarning = PoundsSterling(amount: 1000.0)
     
     func compareEarnings() {
         print(dollarEarning == dollarEarning)
+        print(dollarEarning.amount.value, poundEarning.amount)
         print(dollarEarning.amount == dollarEarning.amount)
     }
 }
